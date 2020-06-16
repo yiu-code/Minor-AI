164d64f8fe3bfa05094830e2b4f5279bbe8226e0
Lou-Anne <0950196@hr.nl>
2020-06-16 13:18:53 +0200

    Hello there - General kenobi

--- PoseNetYoga/poseyogasite/routes/index.js
+++ PoseNetYoga/poseyogasite/routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let listPoses = callJson();
+  res.render('index', {listPoses: listPoses});
 });
 
 module.exports = router;
